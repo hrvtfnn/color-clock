c8f3f355d0c720cba7938648bf16c153e9f5451c
hrvtfnn <hrvtfnn@gmail.com>
2023-03-27 18:08:18 +0200

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function colorClock(){
+    var date = new Date();
+
+    var hours = date.getHours();
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+  
+    if (hours <= 9) {
+      hours = '0' + hours;
+    }
+    if (minutes <= 9) {
+      minutes = '0' + minutes;
+    }
+    if (seconds <= 9) {
+      seconds = '0' + seconds;
+    }
+  
+    var clockFace = hours + ':' + minutes + ':' + seconds;
+    var hexColor = '#' + seconds + minutes + seconds;
+  
+    document.getElementById('clock').innerHTML = clockFace;
+
+    setTimeout(function(){
+        colorClock();
+    }, 1000);
+
+    document.body.style.background = hexColor;
+  }
+
+colorClock();
